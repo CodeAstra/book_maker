c424dad0c63ddd62be85eb6e29b2de0880340dba
B V Satyaram <bvsatyaram@gmail.com>
2016-04-28 18:00:56 +0530

    Show flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,20 @@ module ApplicationHelper
   def landing_page?
     controller_name == "home" && action_name == "land"
   end
+
+  def flash_messages_tag
+    str = ""
+
+    flash.each do |fl|
+      fl[0] = "success" if fl[0] == "notice"
+      fl[0] = "danger" if fl[0] == "error"
+      str += content_tag(:div, class: "alert alert-#{fl[0]}") do
+        content_tag(:button, class: "close", data: {dismiss: "alert"}) do
+          content_tag(:span, raw("&times;"))
+        end + fl[1]
+      end
+    end
+
+    return raw(str)
+  end
 end
