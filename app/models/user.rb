5972cf99616b61ad0fe2d001f02b25a3459b9b11
B V Satyaram <bvsatyaram@gmail.com>
2016-05-11 15:53:58 +0530

    Add devise_invitable to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,7 +24,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :confirmable, :database_authenticatable, :registerable,
+  devise :invitable, :confirmable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :owned_books, class_name: Book.name, foreign_key: :owner_id
